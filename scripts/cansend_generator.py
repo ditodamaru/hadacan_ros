7ae140a4ffa230d5d614388952f32e047129004b
ditodamaru <58300987+ditodamaru@users.noreply.github.com>
2024-03-15 03:58:02 +0900

    added cansend_gen.py

--- scripts/cansend_generator.py
+++ scripts/cansend_generator.py
@@ -9,6 +9,7 @@ import subprocess
 # Constants
 OFFSET1 = 30
 OFFSET2 = 1000
+#JOYSTICK_DEVICE = '/dev/input/js0'
 
 def radian_to_degrees(radian):
     return radian * 180 / 3.14
@@ -45,8 +46,15 @@ def send_cansend_frame(cansend_frame):
         subprocess.check_call(['cansend', 'can0', cansend_frame])
     except subprocess.CalledProcessError as e:
         rospy.logerr("Error sending cansend frame. Return code: %s", e.returncode)
+        rospy.logerr("Sending emergeny stop frame.")
+        stop_frame = generate_cansend_frame(0, 0)  #Frame for stopping the robot
+        #subprocess.call(['cansend', 'can0', stop_frame])  #blocking call (synchronuse process) to stop the robot
+        process = subprocess.Popen(['cansend', 'can0', stop_frame])
     except Exception as e:
         rospy.logerr("Error sending cansend frame: %s", str(e))
+
+# block to detect joystick_connection
+#def check_joystick_connection():        
 
 def ackermann_callback(data):
     steering_angle_rad = data.drive.steering_angle
